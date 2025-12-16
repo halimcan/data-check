f40ba0f23a2f91594d6dbaa19e6cdc3a75bd7f0f
halimcan <albayhalim@gmail.com>
2025-12-16 13:47:17 +0300

    Solved the first challenge of Workintech

--- demo.py
+++ demo.py
@@ -1,5 +1,12 @@
+"""Module providing circle area calculation."""
 
+import math
 
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    """Return the area of a circle given its radius.
+
+    Returns 0 if radius is negative.
+    """
+    if radius < 0:
+        return 0
+    return round(math.pi * radius ** 2, 2)
